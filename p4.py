dbb713b727a5ea092f41ed2b0a337a4e8ddf2649
laseguue <ar.seguret@gmail.com>
2023-04-18 14:42:43 +0200

    class joueur

---
+++ p4.py
@@ -0,0 +1,13 @@
+import os
+import json
+import random
+
+class Joueur:
+    def __init__(self, nom, prenom, date_naissance, numero_national_echec):
+        self.nom = nom
+        self.prenom = prenom
+        self.date_naissance = date_naissance
+        self.numero_national_echec = numero_national_echec
+        self.points = 0
+
+
